6d43576c06772cdf26ed3443f87a61007eda03cb
Memoyu <mmy6076@outlook.com>
2021-04-13 15:16:00 +0800

    feat：添加日志控制器

---
+++ src/mbill_service/Controllers/Core/LogController.cs
@@ -0,0 +1,18 @@
+﻿using mbill_service.Core.Domains.Common.Consts;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mbill_service.Controllers.Core
+{
+    /// <summary>
+    /// 日志管理
+    /// </summary>
+    [Route("api/admin/log")]
+    [ApiExplorerSettings(GroupName = SystemConst.Grouping.GroupName_v2)]
+    public class LogController : ApiControllerBase
+    {
+    }
+}
