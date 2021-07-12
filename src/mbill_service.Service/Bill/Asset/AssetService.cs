8289a8fe2c5183018909e7c53e58ebe272935c09
Memoyu <mmy6076@outlook.com>
2021-07-13 00:52:30 +0800

    fix:优化引用

--- src/mbill_service.Service/Bill/Asset/AssetService.cs
+++ src/mbill_service.Service/Bill/Asset/AssetService.cs
@@ -15,8 +15,6 @@ using mbill_service.Core.Domains.Common;
 using mbill_service.Core.Domains.Common.Consts;
 using mbill_service.Core.Extensions;
 using mbill_service.Service.Bill.Asset.Input;
-using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 
 namespace mbill_service.Service.Bill.Asset
 {
