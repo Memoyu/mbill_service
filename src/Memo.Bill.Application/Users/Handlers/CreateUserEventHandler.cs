8aa144c5f0bd29de4e408ac4be7fa9a12df7edca
Memoyu <mmy6076@outlook.com>
2026-06-29 22:07:41 +0800

    🐛 创建默认账本时创建人赋值为当前用户

--- src/Memo.Bill.Application/Users/Handlers/CreateUserEventHandler.cs
+++ src/Memo.Bill.Application/Users/Handlers/CreateUserEventHandler.cs
@@ -14,6 +14,7 @@ public class CreateUserEventHandler(IBaseDefaultRepository<Ledger> ledgerRepo) :
             Name = "日常账本",
             Color = 0,
             Default = true,
+            CreateUserId = notification.UserId,
         };
         ledger.AddDomainEvent(new CreateLedgerEvent(ledgerId, notification.UserId));
         await ledgerRepo.InsertAsync(ledger, cancellationToken);
